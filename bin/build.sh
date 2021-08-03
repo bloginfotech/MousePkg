4115c8623c08963b26c928cbf7207e6bc56d8689
bloginfotech <88288940+bloginfotech@users.noreply.github.com>
2021-08-03 21:05:43 +0200

    Delete bin directory

--- bin/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /home/jeff/edk/latest/edk2
-. edksetup.sh
-
-build -a X64 -t GCC5 -p /home/jeff/wrk/MousePkg/MousePkg.dsc
-
--- bin/copy_efi_file_to_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-INFNAME=`grep /home/jeff/wrk/MousePkg/*.inf /home/jeff/wrk/MousePkg/MousePkg.dsc`
-EFINAME=`grep BASE_NAME $INFNAME | cut -f2 -d'=' |sed 's/ //g'`
-
-
-cp "/home/jeff/wrk/MousePkg/$EFINAME/DEBUG/$EFINAME.efi" ~/wrk/test/
-
-
